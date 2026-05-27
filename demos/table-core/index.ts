cfc644722a51a5c06e8abd2515b495a2301a988c
Yehuda Katz <wycats@gmail.com>
2026-05-26 23:05:26 -0700

    Add reactive table demo (#311)
    * Add reactive table demo
    
    * Address React demo review comments

---
+++ demos/table-core/index.ts
@@ -0,0 +1,17 @@
+export type {
+  InventoryCategory,
+  InventoryItem,
+  InventoryStats,
+  InventoryViewOptions,
+  RowId,
+  TableRow,
+  TableView,
+  ViewOptions,
+} from "./src/table.js";
+export {
+  createInventory,
+  createTable,
+  Inventory,
+  LOW_STOCK_THRESHOLD,
+  Table,
+} from "./src/table.js";
